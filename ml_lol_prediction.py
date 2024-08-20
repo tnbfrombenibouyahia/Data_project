fbac5b7469147c4217e028f0a9be33b3b5fb7bf3
tnbfrombenibouyahia <theonaimbenhellalpro@gmail.com>
2024-08-20 17:29:31 +0200

    Modifiction du fichier

--- ml_lol_prediction.py
+++ ml_lol_prediction.py
@@ -10,16 +10,50 @@ def scrape_league_data(url): # create the function
       
       return html_page
 
+
 def parse_teams_names(html_page): 
-      pattern = re.compile(r'.* stats in .* 2022')
+      pattern = re.compile(r'.* stats in .* 2024')
       league_table = html_page.find_all(title=pattern)
 
-      zizizcacacacacaca
-      # je mange des pates
+      teams = []
+      
+      for team in league_table:
+          team_name = team.text
+          teams.append(team_name)
 
+      return teams
 
-def main():  # function main get the url_lec and storie in into page
-      html_page = scrape_league_data(url_lec)
-      print(html_page)
+             
+def parse_teams_stats(html_page):
+      team_rows = html_page.find_all('tr')[1:]
+      teams_stats = []
+
+      for row in team_rows: 
+          data_cells = row.find_all('td', class_='text-center')
+          for data_cell in data_cells:
+               stats = data_cell.text.strip()
+               teams_stats.append(stats)
+
+      return teams_stats
+
+def combine_data(teams, teams_stats):
+    result = []
+
+    for i in range(len(teams)):
+        name = teams[i]
+        start_index = i * 5
+        stats_for_name = teams_stats[start_index:start_index+5]
+        name_and_stats = [name] + stats_for_name
+        result.append(name_and_stats)
+
+    return result
+
+def main():
+    html_page = scrape_league_data(url_lec)
+    teams = parse_teams_names(html_page)
+    teams_stats = parse_teams_stats(html_page)
+    result = combine_data(teams, teams_stats)
+
+    print(result)
 
 main()
